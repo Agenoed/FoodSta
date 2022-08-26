7db2e963f6053f5b8edb9f54a534f158665d5d53
Oleksandr Deoneha <oleksandr.deoneha@gmail.com>
2022-08-26 11:01:18 +0300

    add swiper

---
+++ src/js/swiper.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'vertical',
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
