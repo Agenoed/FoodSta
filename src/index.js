ee85035466e79a3197f64b7ebe4b8f47cfd98e21
Oleksandr Deoneha <oleksandr.deoneha@gmail.com>
2022-09-01 17:32:18 +0300

    update js

--- src/index.js
+++ src/index.js
@@ -2,3 +2,7 @@ import $ from 'jquery';
 import 'slick-carousel';
 
 $('.carousel').slick();
+import './js/phone.js';
+import './js/mobile-menu.js';
+import './js/modal-form.js';
+import './js/button-scroll.js';
