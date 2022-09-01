18345e8a21942beb1230b55b98c77b645514059d
Agenoed <oleksandr.deoneha@gmail.com>
2022-09-01 14:24:27 +0000

    Deploying to gh-pages from @ Agenoed/FoodSta@41bfa3309ee9c756c11abd8de5d9ab77b429d56a 🚀

---
+++ index.35360d0b.js
@@ -0,0 +1,2 @@
+function changeHeaderBackground(){var n=document.querySelector("[data-button-up]"),e=n.offsetHeight;window.pageYOffset>e?n.classList.add("button-up--no-transparency"):n.classList.remove("button-up--no-transparency")}window.onscroll=function(){return changeHeaderBackground()};
+//# sourceMappingURL=index.35360d0b.js.map
